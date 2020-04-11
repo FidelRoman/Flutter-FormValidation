4a144d2056e520413c8504266231f33133fc2c0c
Fidel <fidel.roman@pucp.pe>
2020-04-10 19:12:24 -0500

    Mantener la data de los streams después de un hot reload

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -4,10 +4,28 @@ export 'package:formvalidation/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget{
 
+  static Provider _instancia;
+
+  // La idea del factory es determinar si necesito regresar 
+  // una nueva instancia de la clase o utilizar la existente
+  factory Provider({Key key, Widget child}) {
+
+    if ( _instancia == null ) {
+      _instancia = new Provider._internal(key: key, child: child);
+    }
+    return _instancia;
+
+  }
+  // Obtiene argumentos y los envia al factory
+  Provider._internal({Key key, Widget child})
+   : super(key: key, child: child);
+
+  // En un hot reload aquí perdemos la información, por que se crea de nuevo
+  // Solución: crear un singleton
   final loginBloc = LoginBloc();
 
-  Provider({Key key, Widget child})
-  : super(key: key, child: child);
+  // Provider({Key key, Widget child})
+  // : super(key: key, child: child);
 
   @override
   // Al actualizarce debe notificar a sus hijos?
